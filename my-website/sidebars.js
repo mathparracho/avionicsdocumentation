f2e5580ecfa7b4c217b694f9d50b5c0662ab1e03
Gabriel Aleks <gabriel.aleks@nano.ufrj.br>
2021-04-29 15:18:32 -0300

    added reuniao_0428

--- my-website/sidebars.js
+++ my-website/sidebars.js
@@ -6,7 +6,8 @@ module.exports = {
                     '2021': ['reuniao/21/0108r', 'reuniao/21/0113r', 'reuniao/21/0120r', 'reuniao/21/0127r',
                              'reuniao/21/0210r', 'reuniao/21/0217r', 'reuniao/21/0224r', 'reuniao/21/0303r',
                              'reuniao/21/0310r', 'reuniao/21/0317r', 'reuniao/21/0325r', 'reuniao/21/0331r',
-                             'reuniao/21/0401i', 'reuniao/21/0407r', 'reuniao/21/0414r', 'reuniao/21/0422r'
+                             'reuniao/21/0401i', 'reuniao/21/0407r', 'reuniao/21/0414r', 'reuniao/21/0422r',
+                             'reuniao/21/0428r'
                     ],    
                 },
             ],
